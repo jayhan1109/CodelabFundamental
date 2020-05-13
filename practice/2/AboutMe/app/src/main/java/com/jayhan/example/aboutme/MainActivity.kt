dd38d67cb749c72b9d90f9f01b63d5eced1f4c62
Jeongho Han <jungho1109@gmail.com>
2020-05-13 15:53:54 +0900

    5th review Finished SleepTracker tutorial (Coroutine, Room)

---
+++ practice/2/AboutMe/app/src/main/java/com/jayhan/example/aboutme/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.jayhan.example.aboutme
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.jayhan.example.aboutme.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Jay Han")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
+
+        binding.doneButton.setOnClickListener {
+            addNickname()
+        }
+
+        binding.nicknameText.setOnClickListener {
+            updateNickname()
+        }
+
+    }
+
+    private fun addNickname() {
+
+        binding.apply {
+            myName.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(doneButton.windowToken, 0)
+        }
+
+    }
+
+    private fun updateNickname() {
+        binding.apply {
+            nicknameEdit.visibility = View.VISIBLE
+            doneButton.visibility = View.VISIBLE
+            nicknameText.visibility = View.GONE
+
+            nicknameEdit.requestFocus()
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(nicknameEdit, 0)
+        }
+
+    }
+}
---
+++ practice/2/AboutMe/app/src/main/java/com/jayhan/example/aboutme/MyName.kt
@@ -0,0 +1,3 @@
+package com.jayhan.example.aboutme
+
+data class MyName(var name:String="",var nickname:String="")
